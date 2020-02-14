a4ba3a9cc56caeb16a4b3124785fdc8df3c9a85c
Puzant <PBAKJEJIAN@hotmail.com>
2020-02-14 21:09:12 +0200

    css animation

--- src/components/JobForm.vue
+++ src/components/JobForm.vue
@@ -304,4 +304,12 @@ input[type="file"]
     width: 100%
     flex-direction: row
     justify-content: space-around
+
+//  animations 
+.fade-enter-active, .fade-leave-active 
+  transition: opacity .5s
+
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ 
+  opacity: 0
+
 </style>
