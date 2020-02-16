4957cb855774d77ce3ab648a190f5902f6ba58ec
Puzant <PBAKJEJIAN@hotmail.com>
2020-02-16 15:04:12 +0200

    use sweetAlert.js globaly

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { makeServer } from "./server"
+import VueSwal from 'vue-swal'
+
 
 Vue.config.productionTip = false
+Vue.use(VueSwal)
+
 
 if (process.env.NODE_ENV === "development") {
   makeServer()
