fc509ceca799a0e9a3700c2745fe3024124bd753
Puzant <PBAKJEJIAN@hotmail.com>
2020-02-16 15:04:41 +0200

    remove test .get route

--- src/server.js
+++ src/server.js
@@ -6,21 +6,6 @@ export function makeServer({ environment = 'development' } = {}) {
   
     routes() {
       this.namespace = "api"
-  
-      this.get('/test-route', () => {
-        return {
-          movies: [
-            { id: 1, name: "Inception", year: 2010 },
-            { id: 2, name: "Interstellar", year: 2014 },
-            { id: 3, name: "Dunkirk", year: 2017 },
-          ],
-        }
-      })
-
-      //  mock route to post the job form data 
-      this.post('/submit-application', (req, res) => {
-        return 'application recivied'
-      })
       
       this.post('/submit-application', (schema, request) => {
         let application = JSON.parse(request.requestBody)
